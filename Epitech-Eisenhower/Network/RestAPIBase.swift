e714a258ca44edfd11380a7454db485503be408d
ODET Alexandre <madddogg39@gmail.com>
2018-03-30 10:41:08 +0200

    Set default value to Reachable protocol

--- Epitech-Eisenhower/Network/RestAPIBase.swift
+++ Epitech-Eisenhower/Network/RestAPIBase.swift
@@ -9,35 +9,12 @@
 import Foundation
 import Alamofire
 
-class RestAPIBase: Cancelable {
+class RestAPIBase: Cancelable, Reachable {
     
     var request: Alamofire.Request?
     var headers: [String:String] = ["Authorization":""]
     
     func cancelRequest() {
         request?.cancel()
-    }
-}
-
-import SystemConfiguration
-
-extension RestAPIBase: Reachable {
-    var isNetworkAvailable: Bool {
-        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
-        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
-        zeroAddress.sin_family = sa_family_t(AF_INET)
-        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
-            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
-                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
-            }
-        }
-        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
-        if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
-            return false
-        }
-        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
-        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-        let ret = (isReachable && !needsConnection)
-        return ret
     }
 }
